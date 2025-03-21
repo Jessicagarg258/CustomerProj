45e2ac7f52ee21007c38096400c03dad0079c54f
Jessica <158276794+Jessicagarg258@users.noreply.github.com>
2025-03-21 22:14:08 +0530

    Update CustRepository.java

--- custproject/src/main/java/com/mph/custproject/repository/CustRepository.java
+++ custproject/src/main/java/com/mph/custproject/repository/CustRepository.java
@@ -1,5 +1,11 @@
-package com.mph.custproject.repository;
+package com.mph.custproj.repository;
 
-public interface CustRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.mph.custproj.entity.Customer;
+
+@Repository
+public interface CustRepository extends JpaRepository<Customer, Long>{
+	public Customer findByEname(String ename);
 }
