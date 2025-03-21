7c6a9c5da42bd03a1fbb5500c687493d88462bed
Jessica <158276794+Jessicagarg258@users.noreply.github.com>
2025-03-21 22:14:52 +0530

    Update CustServiceImpl.java

--- custproject/src/main/java/com/mph/custproject/service/CustServiceImpl.java
+++ custproject/src/main/java/com/mph/custproject/service/CustServiceImpl.java
@@ -1,5 +1,37 @@
-package com.mph.custproject.service;
+package com.mph.custproj.service;
 
-public class CustServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mph.custproj.entity.Customer;
+import com.mph.custproj.repository.CustRepository;
+
+public class CustServiceImpl implements CustService {
+	
+	@Autowired
+	CustRepository custRepository;
+	@Override
+	public void createEmployee(Customer employee) {
+		custRepository.save(employee);
+		
+	}
+
+	@Override
+	public List<Customer> getAllEmployee() {
+		return custRepository.findAll();
+	}
+
+	@Override
+	public List<Customer> updateEmployee(Customer employee) {
+		custRepository.save(employee);
+		return getAllEmployee();
+	}
+
+	@Override
+	public List<Customer> deleteEmployee(long eid) {
+		custRepository.deleteById(eid);
+		return getAllEmployee();
+	}
 
 }
