9e5a4c44054d04500bc6ec5b3eba253c4b092e63
Jessica <158276794+Jessicagarg258@users.noreply.github.com>
2025-03-21 22:14:29 +0530

    Update CustService.java

--- custproject/src/main/java/com/mph/custproject/service/CustService.java
+++ custproject/src/main/java/com/mph/custproject/service/CustService.java
@@ -1,5 +1,15 @@
-package com.mph.custproject.service;
+package com.mph.custproj.service;
+
+import java.util.List;
+
+
+import com.mph.custproj.entity.Customer;
+
 
 public interface CustService {
-
+	public void createEmployee(Customer employee);
+	public List<Customer> getAllEmployee() ;
+	public List<Customer> updateEmployee(Customer employee);
+	public List<Customer> deleteEmployee(long eid);
+	
 }
